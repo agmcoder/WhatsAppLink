02aebe1f05b6e097b6f0ed4815c30450d3f3ffad
alejandro.gomez <alejandro.gomez@external.wolterskluwer.com>
2022-09-19 17:27:09 +0200

    first commit for api call

--- WhatsAppLinkAndroid/src/main/java/com/example/whatsapplink/android/service/WhatsAppAPI.kt
+++ WhatsAppLinkAndroid/src/main/java/com/example/whatsapplink/android/service/WhatsAppAPI.kt
@@ -1,4 +1,26 @@
 package com.example.whatsapplink.android.service
 
 class WhatsAppAPI {
+    //properties
+    private val baseURLSpain : String = "https://api.whatsapp.com/send?phone=34"
+
+
+    //functions
+
+
+
+
+
+
+
+
+
+
+    //this function is used to create the link to send a message to a number
+    fun getWhatsAppLinkSpain(phoneNumber : String) : String {
+        return baseURLSpain + phoneNumber
+    }
+
+
+
 }
